624c19057518e4a5cb3a1307c970a13363186f51
Jethro Lim <jethro.elijahlim01@gmail.com>
2025-04-20 22:25:23 +0700

    Parser split comma

--- phase_1/backend/services/parser.py
+++ phase_1/backend/services/parser.py
@@ -20,115 +20,33 @@ def parse_address(address, data):
     if pd.isna(address):
         return places
 
-    cities = list(data['city'])
-    states = list(data['state'])
-    cities_ori = list(data['city_ori'])
-    states_ori = list(data['state_ori'])
+    
+def parse_address(address, data):
+    places = pd.DataFrame()
+    places['Address'] = [address]
+    places['Street'] = pd.NA
+    places['City'] = pd.NA
+    places['State'] = pd.NA
 
-    street = address
-    state_get = ''
-    city_get = ''
+    if pd.isna(address):
+        return places
 
-    possible_states = []
-    get_index = []
+    address = address.split(',')
+    if len(address) == 3:
+        places['Address'] = [address]
+        places['Street'] = [address[0]]
+        places['City'] = [address[1].replace(" ", "")]
+        match = data[data['city_ori'] == address[1].replace(" ", "")]
+        places['State'] = match['state_ori'].iloc[0] if not match.empty else None
 
-    for i in range(len(states)):
-        if (states[i] in street.lower() and len(states[i]) > 3):
-            possible_states.append(states[i])
-            get_index.append(i)
+    if len(address) == 2:
+        places['Address'] = [address]
+        places['City'] = [address[0].replace(" ", "")]
+        match = data[data['city_ori'] == address[0].replace(" ", "")]
+        places['State'] = match['state_ori'].iloc[0] if not match.empty else None
 
-    reversed_address = street.lower().split(' ')[::-1]
-    found = 0
-    confident_state = 0
-
-    for i in range(len(reversed_address)):
-        for j in range(len(possible_states)):
-            if re.sub(r'[^\w\s]', '', reversed_address[i]) == possible_states[j]:
-                state_get = possible_states[j]
-                places['State'] = states_ori[get_index[j]]
-                found = 1
-                confident_state = 1
-                break
-            if (possible_states[j] in reversed_address[i]) and (found == 0):
-                state_get = possible_states[j]
-                places['State'] = states_ori[get_index[j]]
-                found = 1
-        if confident_state == 1:
-            break
-
-    street = remove_last_word(street, state_get)
-
-    if confident_state == 1:
-        cities = list(data[data['state'] == state_get]['city'])
-        cities_ori = list(data[data['state'] == state_get]['city_ori'])
-
-    get_index = []
-    possible_cities = []
-
-    for i in range(len(cities)):
-        if (cities[i] in street.lower() and len(cities[i]) > 3):
-            possible_cities.append(cities[i])
-            get_index.append(i)
-
-    found = 0
-    confident_city = 0
-    for i in range(len(reversed_address)):
-        for j in range(len(possible_cities)):
-            if re.sub(r'[^\w\s]', '', reversed_address[i]) == possible_cities[j]:
-                city_get = possible_cities[j]
-                places['City'] = cities_ori[get_index[j]]
-                confident_city = 1
-                found = 1
-                break
-            if (possible_cities[j] in reversed_address[i]) and (found == 0):
-                city_get = possible_cities[j]
-                places['City'] = cities_ori[get_index[j]]
-                found = 1
-        if confident_city == 1:
-            break
-
-    street = remove_last_word(street, city_get)
-
-    reversed_address = address.lower().split(' ')[::-1]
-
-    if (confident_state == 0) and (confident_city == 1):
-        possible_states = list(data[data['city'] == city_get]['state'])
-        states_ori = list(data[data['city'] == city_get]['state_ori'])
-
-        found = 0
-        for i in range(len(reversed_address)):
-            for j in range(len(possible_states)):
-                if re.sub(r'[^\w\s]', '', reversed_address[i]) == possible_states[j]:
-                    state_get = possible_states[j]
-                    places['State'] = states_ori[j]
-                    confident_state = 1
-                    break
-                if (possible_states[j] in reversed_address[i]) and (found == 0):
-                    state_get = possible_states[j]
-                    places['State'] = states_ori[j]
-                    found = 1
-            if confident_state == 1:
-                break
-        
-        street = remove_last_word(street, state_get)
-
-    reversed_address = street.split(' ')[::-1]
-
-    if confident_city == 1 and state_get == '':
-        states = list(data[(data['city'] == city_get) & (data['state'].apply(len) > 1)]['state_code'])
-        states_ori = list(data[(data['city'] == city_get) & (data['state'].apply(len) > 1)]['state_ori'])
-        for i in range(len(reversed_address)):
-            for j in range(len(states)):
-                if re.sub(r'[^\w\s]', '', reversed_address[i].upper()) == states[j]:
-                    state_get = states_ori[j]
-                    street = remove_last_word(street, state_get)
-                    places['State'] = state_get
-                    break
-            if state_get != '':
-                break
-
-    street = re.sub(r'(,\s*)+', ', ', street.strip())
-    places['Street'] = re.sub(r'[, ]+$', '', street)
+    else:
+        places['Street'] = [address[0]]
 
     return places
 
@@ -180,14 +98,14 @@ def parse_data(scraped, data):
     scraped['Business_phone'] = scraped['Business_phone'].apply(parse_number)
 
     # Merge parsed address data
-    scraped = pd.merge(scraped, address_df, on='Address', how='inner')
+    scraped = pd.concat([scraped.reset_index(drop=True), address_df.reset_index(drop=True)], axis=1)
 
     # Save output
     scraped.to_csv('../data/Parsed_output.csv', index=False)
 
 
 if __name__ == "__main__":
-    data = pd.read_csv('normalized_city_state_dataset.csv')
+    data = pd.read_csv('normalized_city_state_dataset_us.csv')
     data.dropna(inplace=True)
     scraped = pd.read_csv('merged_output.csv')
     parse_data(scraped, data)
