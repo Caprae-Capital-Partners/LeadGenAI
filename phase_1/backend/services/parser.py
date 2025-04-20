e1ebff5b4ad89d4be24ef589b5223cb333e13186
Yash J <yj@gmail.com>
2025-04-21 00:31:26 +0530

    Removed unused param and improved type hints

--- phase_1/backend/services/parser.py
+++ phase_1/backend/services/parser.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import re
-from collections import defaultdict
 
 def remove_last_word(text, word):
     matches = list(re.finditer(rf'\b{re.escape(word)}\b', text, flags=re.IGNORECASE))
@@ -10,7 +9,7 @@ def remove_last_word(text, word):
         text = text[:start] + text[end:]
     return re.sub(r'(,\s*)+', ', ', text.strip())
     
-def parse_address(address, data):
+def parse_address(address: str) -> pd.DataFrame:
     places = pd.DataFrame()
     places['Address'] = [address]
     places['Street'] = pd.NA
@@ -30,7 +29,6 @@ def parse_address(address, data):
     if len(address) == 2:
         places['Address'] = [address]
         places['City'] = [address[0].replace(" ", "")]
-        match = data[data['city_ori'] == address[0].replace(" ", "")]
         places['State'] = re.sub(r'[\d\s\-]', '', address[1])
 
     else:
@@ -38,7 +36,7 @@ def parse_address(address, data):
 
     return places
 
-def parse_number(raw):
+def parse_number(raw: str) -> str:
     # Remove country code and keep digits
     if pd.isna(raw):
         return raw
@@ -55,29 +53,10 @@ def parse_number(raw):
     last = local[6:]
     
     return f"({area})-{mid}-{last}"
-
-# def parse_data(scraped, data):
-#     address_column = list(scraped['Address'])
-#     phone_column = list(scraped['Business_phone'])
-    
-#     address_df = pd.DataFrame()
-#     for i in address_column:
-#         address_df = pd.concat([address_df, parse_address(i, data)], axis = 0)
-    
-#     phone_list = []
-#     for i in phone_column:
-#         phone_list.append(parse_number(i))
-        
-#     scraped['Business_phone'] = phone_list
-#     scraped = pd.merge(scraped, address_df, on='Address', how='inner')
-    
-#     scraped.to_csv('../data/Parsed_output.csv', index=False)
-    
-    # return scraped #if needed
-    
-def parse_data(scraped, data):
+   
+def parse_data(scraped: pd.DataFrame) -> pd.DataFrame:
     # Apply parse_address to each address and collect into list of DataFrames
-    address_dfs = [parse_address(address, data) for address in scraped['Address']]
+    address_dfs = [parse_address(address) for address in scraped['Address']]
     
     # Combine all address DataFrames at once
     address_df = pd.concat(address_dfs, ignore_index=True)
@@ -102,14 +81,10 @@ def parse_data(scraped, data):
         
     scraped = scraped.drop(columns='Address')
 
-    # Save output
-    scraped.to_csv('../data/Parsed_output.csv', index=False)
+    # scraped.to_csv('../data/Parsed_output.csv', index=False)
+    return scraped
 
 
-if __name__ == "__main__":
-    data = pd.read_csv('normalized_city_state_dataset_us.csv')
-    data.dropna(inplace=True)
-    scraped = pd.read_csv('merged_output.csv')
-    parse_data(scraped, data)
-    # print(parse_address('1300 E 86th St Ste 14 Box 130, Indianapolis, IN 46240-1910', data))
-    # print(parse_number('+1 317-241-4327'))
+# if __name__ == "__main__":
+#     scraped = pd.read_csv('merged_output.csv')
+#     parse_data(scraped)
