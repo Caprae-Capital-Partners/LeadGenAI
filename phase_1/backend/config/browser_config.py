19c35140fd098eb5bc3d0697b596357a69d79244
Yash J <yj@gmail.com>
2025-04-20 23:33:04 +0530

    Adding launch arguments and browser context

--- phase_1/backend/config/browser_config.py
+++ phase_1/backend/config/browser_config.py
@@ -12,12 +12,15 @@ class PlaywrightManager:
     async def start_browser(self, stealth_on: bool):
         """Initialize the Playwright session and start the browser."""
         self.playwright = await async_playwright().start()
-        self.browser = await self.playwright.chromium.launch(headless=self.headless)
+        self.browser = await self.playwright.chromium.launch(headless=self.headless,
+                                                             args= ['--disable-dev-shm-usage'])
         self.context = await self.browser.new_context(
-            viewport={"width": 1280, "height": 800},
+            viewport={"width": 1920, "height": 1080},
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             java_script_enabled=True,
             ignore_https_errors=True,
+            bypass_csp=True,
+            locale='en-US',
         )
         self.page = await self.context.new_page()
         if stealth_on:
