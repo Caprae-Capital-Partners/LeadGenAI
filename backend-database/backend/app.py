e05ffce83a1fff9288f296f1f637e8989cb2ec89
Rafiiisy <rafisyafrinaldi17@gmail.com>
2025-05-27 14:09:09 +0700

    improve handling for authorization

--- backend-database/backend/app.py
+++ backend-database/backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, has_request_context, g
+from flask import Flask, has_request_context, g, jsonify, request, redirect, url_for
 from models.lead_model import db
 from routes.lead_routes import lead_bp
 from routes.main_routes import main_bp
 from routes.auth_routes import auth_bp
 from routes.lead_access_routes import access_bp
 from config.config import config
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, request
 from models.user_model import User
 from sqlalchemy import event, text
 from flask_cors import CORS
@@ -44,6 +44,14 @@ def create_app(config_class=config):
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
     login_manager.login_message_category = 'info'
+
+
+    @login_manager.unauthorized_handler
+    def handle_unauthorized():
+        if request.path.startswith("/api/"):
+            return jsonify({"error": "Unauthorized"}), 401
+        return redirect(url_for('auth.login'))
+    
 
     @login_manager.user_loader
     def load_user(user_id):
