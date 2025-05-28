0e6a92bcb6cf4d5adcf27be82d8c781538b7143f
Rafiiisy <rafisyafrinaldi17@gmail.com>
2025-05-28 16:38:41 +0700

    prettify subscription page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -91,12 +91,17 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeInDown: {
+          "0%": { opacity: 0, transform: "translateY(-20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in-down": "fadeInDown 0.6s ease-out both",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
