5080da06a691ea5fb135c97de1ec0aa21a02a353
cherzs <zhafrang638@gmail.com>
2025-06-05 19:55:43 +0700

    change

--- backend-database/backend/app.py
+++ backend-database/backend/app.py
@@ -94,4 +94,4 @@ def create_app(config_class=config):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=2025)
+    app.run(debug=True, host='0.0.0.0', port=8003)
