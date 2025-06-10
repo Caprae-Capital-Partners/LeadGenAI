6d5c62c87d9cbad81bcc8e99fb0e502594544781
Rafiiisy <rafisyafrinaldi17@gmail.com>
2025-06-10 10:06:00 +0700

    minor fix

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -14,8 +14,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
-
+const DATABASE_URL = process.env.NEXT_PUBLIC_DATABASE_URL;
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL_P2;
 export function Header() {
   const [userEmail, setUserEmail] = useState("");
